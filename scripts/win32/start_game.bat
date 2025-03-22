c3742f77546b101b4fb603c55dc6b8e0fdfd774b
Vitor26laser <97393601+Vitor23laser@users.noreply.github.com>
2025-03-22 11:02:21 -0300

    Update start_game.bat

--- scripts/win32/start_game.bat
+++ scripts/win32/start_game.bat
@@ -1,15 +1,11 @@
 @echo off
-title Open Toontown - Game Client
-cd ..
-cd ..
-cd src
+title Toontown BR QA- Game Client
+cd..
 
 rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
 set /P PPYTHON_PATH=<PPYTHON_PATH
-rem Set default value if PPYTHON_PATH is empty:
-if "%PPYTHON_PATH%"=="" set PPYTHON_PATH=python
 
 set LOGIN_TOKEN=dev
 
-%PPYTHON_PATH% main.py --token %LOGIN_TOKEN%
+%PPYTHON_PATH% -m dumped.toontown.launcher.StartToontownLauncher
 pause
